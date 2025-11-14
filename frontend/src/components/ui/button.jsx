1e9974a47226a1b62285247764cd3453b51e5b0a
amnepka <amnepka@gmail.com>
2025-11-14 07:48:23 +0000

    auto-commit for 3a6cbc16-5064-46cd-ab68-75d7a5505d1c

--- frontend/src/components/ui/button.jsx
+++ frontend/src/components/ui/button.jsx
@@ -24,8 +24,9 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-lg px-3 text-xs",
         lg: "h-11 rounded-lg px-8",
+        xl: "h-12 rounded-lg px-8 text-base", 
         icon: "h-10 w-10",
-        "icon-lg": "h-12 w-12", // Добавьте этот размер
+        "icon-lg": "h-12 w-12", 
       },
     },
     defaultVariants: {
